6038e14387ee0846dd5f3e100690c3bd02d2137e
Curtis A. Liu <Curtisaliu@gmail.com>
2022-04-13 16:46:47 -0700

    completed home page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react"
+import Home from './pages/Home'
+import Header from './Header'
+
 import {
   BrowserRouter as Router,
   Route,
@@ -7,21 +10,15 @@ import {
 
 class App extends Component {
   render() {
-    const {
-      logged_in,
-      current_user,
-      new_user_route,
-      sign_in_route,
-      sign_out_route
-    } = this.props
-    console.log("logged_in:", logged_in)
-    console.log("current_user:", current_user)
-    console.log("new_user_route:", new_user_route)
-    console.log("sign_in_route:", sign_in_route)
-    console.log("sign_out_route:", sign_out_route)
     return (
       <>
-        <h1>Apartment App</h1>
+        <Router>
+          <Header {...this.props} />
+          {/* <Navigation /> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
       </>
     )
   }
